d0b9e8e6b30fd7df2f3b86bbda5d2571bfd83c7b
TinaHeiligers <tina.heiligers@gmail.com>
2017-01-23 11:22:44 -0700

    added stuff

--- app.js
+++ app.js
@@ -1,14 +1,126 @@
 var express = require('express');
-// import express from 'express';
 var bodyParser = require('body-parser');
-// import bodyParser from 'body-parser';
 var path = require('path');
-// import path from 'path';
+var expressValidator = require('express-validator');
 
 var app = express();
 
+//MIDDLEWARE-must come before routing
+// var logger = (req, res, next) => {
+//   console.log('Logging...');
+//   next();
+// }
+// app.use(logger);
+
+// MIDDLEWARE View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//MIDDLEWARE body parser 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//MIDDLEWARE Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//GLOBAL VARS
+app.use((req, res, next) => {
+  res.locals.errors = null;
+  next();
+});
+
+//MIDDLEWARE for Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//parsing JSON
+// var people = [
+//   {
+//     name: 'Tina',
+//     age: 40
+//   },
+//   {
+//     name: 'Marc',
+//     age: 41
+//   },
+//   {
+//     name: 'Michael',
+//     age: 8
+// }
+// ]
+
+//PARSING JSON through routing
+// app.get('/', (req, res) => {
+//   res.json(people);
+// });
+
+var users = [
+  {
+    id: 1,
+    first_name: 'Tina',
+    last_name: 'Heiligers',
+    email: 'tina.heiligers@gmail.com'
+  },
+  {
+    id: 2,
+    first_name: 'Marc',
+    last_name: 'Heiligers',
+    email: 'marc.heiligers@gmail.com'
+  },
+  {
+    id: 3,
+    first_name: 'Bella',
+    last_name: 'Heiligers',
+    email: 'bella.heiligers@gmail.com'
+  }
+]
+
+//ROUTER
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index', {
+    title: 'ScrapBook Users',
+    users: users
+  });
+});
+
+app.post('/users/add', (req, res) => {
+  //1. npm install express-validator, requrie at top
+  //2. using express validator: set validation checks
+  req.checkBody('first_name', 'First Name is required').notEmpty();
+  req.checkBody('last_name', 'Last Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  //3. create variable to hold errors:
+  var errors = req.validationErrors();
+  
+  if(errors){
+    //rerender the same page if there are errors with the forms submission
+    res.render('index', {
+      title: 'ScrapBook Users',
+      users: users,
+      errors: errors
+    });
+  } else {
+      var newUser = {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email
+    }  
+    console.log('SUCCESS');
+  }
 });
 
 app.listen(3000, () => {
---
+++ public/useOfPublicFolder.md
@@ -0,0 +1,3 @@
+We use the public folder to store all our static files such as css etc. For client side stuff, store it all in the public folder.
+In the case of this little app, we're going to render all views from teh server, so we won't be using this folder.
+If we had an index.html file here, it would override the app.js file as the entry point.
